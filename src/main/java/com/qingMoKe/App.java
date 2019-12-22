f8747f93d692a103547bd1ed7cdc1c0547228011
qingMoKe <279213812@qq.com>
2019-12-22 18:33:49 +0800

    Git笔记

---
+++ src/main/java/com/qingMoKe/App.java
@@ -0,0 +1,13 @@
+package com.qingMoKe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
